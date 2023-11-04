90941b601408f14928c4f45f3d59f4fc4c66e3b1
Gregory Machado <gregory2017machado@gmail.com>
2023-11-04 16:18:12 -0300

    commit atualização de trecohos e modalidades e dados

--- Amarelinha/src/main/java/ConsultarTrechoEModalidade.java
+++ Amarelinha/src/main/java/ConsultarTrechoEModalidade.java
@@ -1,8 +1,57 @@
 package Amarelinha.src.main.java;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class ConsultarTrechoEModalidade {
 
     public static void consulta(String cidade1, String cidade2, String modalidade) {
-       
+
+
+        class ConsultaTrechos {
+
+            static Map<String, Integer> trechos = new HashMap<>();
+            static Map<String, Double> custos = new HashMap<>();
+
+            static {
+                // Inicialize os trechos e custos aqui
+                trechos.put("PORTO ALEGRE-SÃO PAULO", 1000); // Exemplo
+                custos.put("pequeno", 2.0); // Exemplo
+                custos.put("medio", 3.0);   // Exemplo
+                custos.put("grande", 4.0);  // Exemplo
+            }
+
+            public static double calcularCusto(int distancia, String tipoCaminhao) {
+                return distancia * custos.get(tipoCaminhao);
+            }
+
+            public static void consultarTrecho(String origem, String destino, String tipoCaminhao) {
+                String trecho = origem.toUpperCase() + "-" + destino.toUpperCase();
+                if (trechos.containsKey(trecho)) {
+                    int distancia = trechos.get(trecho);
+                    double custo = calcularCusto(distancia, tipoCaminhao);
+                    System.out.println("A distância entre " + origem + " e " + destino + " é de " + distancia + " km.");
+                    System.out.println("O custo estimado para o trecho é de R$ " + String.format("%.2f", custo));
+                } else {
+                    System.out.println("Um dos trechos informados não está disponível.");
+                }
+            }
+
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.print("Informe a cidade de origem: ");
+                String origem = scanner.nextLine();
+
+                System.out.print("Informe a cidade de destino: ");
+                String destino = scanner.nextLine();
+
+                System.out.print("Informe o tipo de caminhão (pequeno, medio, grande): ");
+                String tipoCaminhao = scanner.nextLine().toLowerCase();
+
+                consultarTrecho(origem, destino, tipoCaminhao);
+            }
+        }
     }
 }
