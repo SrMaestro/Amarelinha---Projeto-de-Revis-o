62b861f6bab66d4083bd70ac0b16a9e6e6b42056
Vin <93566073+V-inn@users.noreply.github.com>
2023-11-07 21:55:45 -0300

    Adicionado função para criar viagens, em segmentos, ainda sem comentários

--- Amarelinha/src/main/java/SegmentoDeViagem.java
+++ Amarelinha/src/main/java/SegmentoDeViagem.java
@@ -3,11 +3,37 @@ package Amarelinha.src.main.java;
 import java.util.Map;
 
 public class SegmentoDeViagem extends NovaCarga {
-    public String cidade;
-    public Map<String, Integer> Carga;
+    private final String cidade;
+    private Map<String, Integer> Carga;
+    private String TagDeEtapa;
+
 
     public SegmentoDeViagem(String cidade, Map<String, Integer> Carga){
         this.cidade = cidade;
         this.Carga = Carga;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public Map<String, Integer> getCarga() {
+        return Carga;
+    }
+
+    public void setCargaZero(String item){
+        this.Carga.replace(item, 0);
+    }
+
+    public void setCarga(Map<String, Integer> Carga){
+        this.Carga = Carga;
+    }
+
+    public void removeCarga(String item){
+        this.Carga.remove(item);
+    }
+
+    public void setTagDeEtapa(String tagDeEtapa) {
+        this.TagDeEtapa = tagDeEtapa;
     }
 }
--- Amarelinha/src/main/java/Viagem.java
+++ Amarelinha/src/main/java/Viagem.java
@@ -4,17 +4,30 @@ import java.util.*;
 
 public class Viagem extends NovaCarga {
     private int distanciaTotal;
-    private final Map<String, Integer> Carga;
+    private Map<String, Integer> Carga;
     private final List<SegmentoDeViagem> segmento = new Stack<>();
     private Map<String, Integer> caminhoes = new HashMap<>();
     private final Double[][] valorPorTrecho = new Double[24][24];
     private double peso;
     private double valor;
 
+    private void descarregar(SegmentoDeViagem segmento){
 
+        for(String s : segmento.getCarga().keySet()){
+            if (!this.Carga.containsKey(s)){
+                segmento.removeCarga(s);
+                System.err.println("Item nao existe na carga inicial.");
+            }else if (segmento.getCarga().get(s) > this.Carga.get(s)){
+                segmento.setCargaZero(s);
+                System.err.println("Quantidade nao pode ser maior do que a de produtos da carga.");
+            }else {
+                this.Carga.replace(s, this.Carga.get(s) - segmento.getCarga().get(s));
+            }
+        }
+    }
 
-    public Viagem(SegmentoDeViagem inicio){
-        this.Carga = inicio.Carga;
+    private void funcaoInicio(SegmentoDeViagem inicio){
+        this.Carga = inicio.getCarga();
         this.peso = NovaCarga.calcularPesoDaCarga(this.Carga);
 
         ObjetoCusto o = Modalidades.calculaMelhorValorInicial(this.peso);
@@ -22,9 +35,28 @@ public class Viagem extends NovaCarga {
         this.caminhoes = o.getCaminhoes();
         this.valor = o.getValor();
 
+        inicio.setTagDeEtapa("INICIO");
         segmento.add(inicio);
-
-
     }
 
+    private void funcaoMeio(SegmentoDeViagem meio){
+        descarregar(meio);
+        meio.setTagDeEtapa("MEIO");
+        segmento.add(meio);
+    }
+
+    private void funcaoFim(SegmentoDeViagem fim){
+        fim.setCarga(this.Carga);
+        descarregar(fim);
+        fim.setTagDeEtapa("FIM");
+        segmento.add(fim);
+    }
+
+    public List<SegmentoDeViagem> Viagem(SegmentoDeViagem inicio, SegmentoDeViagem meio, SegmentoDeViagem fim){
+        this.funcaoInicio(inicio);
+        this.funcaoMeio(meio);
+        this.funcaoFim(fim);
+
+        return segmento;
+    }
 }
